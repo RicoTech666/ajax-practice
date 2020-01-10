79ab4323ea4ff4b3ae7ba306a08d5acd7d672307
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-10 20:18:09 +0800

    [Jiachen] feat: users finished

--- users/ajax.js
+++ users/ajax.js
@@ -1,13 +1,39 @@
-/**
-options = {
-  url: "",
-  method: "",
-  headers: {}, 
-  data: "",
-  success: function(result) {},  // 请求成功后调用此方法
-  fail: function(error) {}    // 请求失败或出错后调用此方法
-}
-**/
-window.ajax = function (options) {
+/* options = {
+	url: "",
+	method: "",
+	headers: {},
+	data: "",
+	success: function(result) {}, // 请求成功后调用此方法
+	fail: function(error) {}, // 请求失败或出错后调用此方法
+}; */
 
-}
+window.ajax = function(options) {
+	const ajaxData = {
+		url: options.url || "",
+		method: options.method.toUpperCase() || "GET",
+		headers: options.headers || {},
+		data: options.data || null,
+		onSuccess: options.success || function(result) {},
+		onFail: options.fail || function(error) {},
+	};
+
+	var xmlhttp = new XMLHttpRequest();
+
+	/* 	xmlhttp.onreadystatechange = function() {
+		if (4 === xmlhttp.readyState) {
+			if (200 === xmlhttp.status || (201 === xmlhttp.status && ajaxData.method === "POST")) {
+				ajaxData.onSuccess(JSON.parse(xmlhttp.responseText));
+			} else {
+				ajaxData.onFail(xmlhttp.status);
+			}
+		}
+  }; */
+	xmlhttp.onload = () => ajaxData.onSuccess(JSON.parse(xmlhttp.responseText));
+	xmlhttp.onerror = () => ajaxData.onFail(xmlhttp.status);
+	xmlhttp.open(ajaxData.method, ajaxData.url, true);
+	if ("POST" === ajaxData.method || ajaxData.method === "PUT") {
+		xmlhttp.setRequestHeader("content-type", "application/json");
+		ajaxData.data = JSON.stringify(ajaxData.data);
+	}
+	xmlhttp.send(ajaxData.data);
+};
--- users/index.js
+++ users/index.js
@@ -1,85 +1,122 @@
-(function () {
-    const API_ROOT = "http://localhost:3000/users";
-    let $userList = document.getElementById('user-list');
-    let $username = document.getElementById('username');
-    let $password = document.getElementById('password');
+(function() {
+	const API_ROOT = "http://localhost:3000/users";
+	let $userList = document.getElementById("user-list");
+	let $username = document.getElementById("username");
+	let $password = document.getElementById("password");
 
-    // TODO： get user list，when success, run renderUserList
-    function getListData() {
+	// TODO： get user list，when success, run renderUserList
+	function getListData() {
+		ajax({
+			url: API_ROOT,
+			method: "GET",
+			success: function(response) {
+				renderUserList(response);
+			}, // 请求成功后调用此方法
+			fail: function(error) {
+				console.log("request fail!");
+			}, // 请求失败或出错后调用此方法
+		});
+	}
 
-    }
+	// TODO:  add user info,when success, run addItem
+	function addItemData(data) {
+		ajax({
+			url: API_ROOT,
+			method: "POST",
+			data: data,
+			success: function(data) {
+				addItem(data);
+			},
+		});
+	}
 
-    // TODO:  add user info,when success, run addItem
-    function addItemData(data) {
+	// TODO: update user info,when success, run updateItem
+	// 提示：Math.random().toString(36).substring(2) 生成随机的字符串
 
-    }
+	function updateItemData(id) {
+		ajax({
+			url: API_ROOT + "/" + id,
+			method: "PUT",
+			data: {
+				username: Math.random()
+					.toString(36)
+					.substring(2),
+				password: Math.random()
+					.toString(36)
+					.substring(2),
+			},
+			success: function(returnedObj) {
+				updateItem(returnedObj);
+			},
+		});
+	}
 
-    // TODO: update user info,when success, run updateItem
-    // 提示：Math.random().toString(36).substring(2) 生成随机的字符串
-    function updateItemData(id) {
+	// TODO: delete user info,,when success, run  deleteItem
+	function deleteItemData(id) {
+		ajax({
+			url: API_ROOT + "/" + id,
+			method: "DELETE",
+			success: function(response) {
+				deleteItem(id);
+			},
+		});
+	}
 
-    }
+	document.getElementById("add-btn").addEventListener("click", function() {
+		let username = document.getElementById("username").value;
+		let password = document.getElementById("password").value;
+		let data = {
+			username: username,
+			password: password,
+		};
+		addItemData(data);
+	});
 
-    // TODO: delete user info,,when success, run  deleteItem
-    function deleteItemData(id) {
+	$userList.addEventListener("click", function(event) {
+		let id = event.target.getAttribute("data-id") || event.target.parentElement.getAttribute("data-id");
 
-    }
+		if (!id) {
+			return false;
+		}
 
-    document.getElementById('add-btn').addEventListener('click', function () {
-        let username = document.getElementById('username').value;
-        let password = document.getElementById('password').value;
-        let data = {
-            "username": username,
-            "password": password
-        }
-        addItemData(data);
-    })
+		switch (true) {
+			case event.target.innerHTML === "X":
+				deleteItemData(id);
+				break;
+			default:
+				updateItemData(id);
+				break;
+		}
+	});
 
-    $userList.addEventListener('click', function (event) {
-        let id = event.target.getAttribute('data-id') || event.target.parentElement.getAttribute('data-id');
+	function renderUserList(data) {
+		if (!Array.isArray(data) && !data instanceof Array) {
+			return false;
+		}
 
-        if (!id) {
-            return false;
-        }
+		$userList.innerHTML = data.reduce((acc, cur) => {
+			return (acc += `<li data-id="${cur.id}"><span>用户名：${cur.username} - 密码：${cur.password}</span><span>X</span></li>`);
+		}, "");
+	}
 
-        switch (true) {
-            case event.target.innerHTML === 'X':
-                deleteItemData(id);
-                break;
-            default:
-                updateItemData(id);
-                break;
-        }
-    })
+	function addItem(item) {
+		var $item = document.createElement("li");
+		$item.setAttribute("data-id", item.id);
+		$item.innerHTML = `<span>用户名：${item.username} - 密码：${item.password}</span><span>X</span>`;
+		$userList.appendChild($item);
+		$username.value = "";
+		$password.value = "";
+	}
 
-    function renderUserList(data) {
-        if (!Array.isArray(data) && !data instanceof Array) {
-            return false;
-        }
+	function updateItem(item) {
+		var $item = $userList.querySelector(`li[data-id='${item.id}']`);
+		$item.innerHTML = `<span>用户名：${item.username} - 密码：${item.password}</span><span>X</span>`;
+	}
 
-        $userList.innerHTML = data.reduce((acc, cur) => {
-            return acc += `<li data-id="${cur.id}"><span>用户名：${cur.username} - 密码：${cur.password}</span><span>X</span></li>`;
-        }, '');
-    }
+	function deleteItem(id) {
+		var $item = $userList.querySelector(`li[data-id='${id}']`);
+		$userList.removeChild($item);
+	}
 
-    function addItem(item) {
-        var $item = document.createElement('li');
-        $item.setAttribute('data-id', item.id);
-        $item.innerHTML = `<span>用户名：${item.username} - 密码：${item.password}</span><span>X</span>`;
-        $userList.appendChild($item);
-        $username.value = '';
-        $password.value = '';
-    }
-
-    function updateItem(item) {
-        var $item = $userList.querySelector(`li[data-id='${item.id}']`);
-        $item.innerHTML = `<span>用户名：${item.username} - 密码：${item.password}</span><span>X</span>`;
-    }
-
-    function deleteItem(id) {
-        var $item = $userList.querySelector(`li[data-id='${id}']`);
-        $userList.removeChild($item);
-    }
-
-    getListData();
+	getListData();
 })();
